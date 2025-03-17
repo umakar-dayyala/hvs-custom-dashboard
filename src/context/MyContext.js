f932d515bd4e0b9a923d96d0223796042f8a8353
pradiphande <pradip.hande@hitachids.com>
2025-03-17 12:21:00 +0530

    context added

---
+++ src/context/MyContext.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+// Create the context
+export const MyContext = createContext();
+
+// Create a provider component
+export const MyProvider = ({ children }) => {
+  const [value, setValue] = useState([]);
+
+  return (
+    <MyContext.Provider value={{ value, setValue }}>
+      {children}
+    </MyContext.Provider>
+  );
+};
