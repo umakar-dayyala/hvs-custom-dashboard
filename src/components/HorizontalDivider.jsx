fff520022085e2c9497155fbc38e3080a3846a50
Vishal Kumar Yadav <vishal.kumaryadav@hitachids.com>
2025-03-21 23:03:58 +0530

    build only

--- src/components/HorizontalDivider.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Divider } from "@mui/material";
-
-const HorizontalDivider = () => (
-  <Divider style={{ border: "1px solid #70707059", margin: "1rem" }} />
-);
-
-export default HorizontalDivider;
--- src/components/VerticalDivider.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Divider } from "@mui/material";
-
-const VerticalDivider = () => (
-    <Divider orientation="vertical" flexItem style={{ backgroundColor: "#CECECE", height: "20px", margin: "0 8px" }} />
-);
-
-export default VerticalDivider;
