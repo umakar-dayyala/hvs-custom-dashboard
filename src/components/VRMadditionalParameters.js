182850e9d9595658507154ad5f4d7e6ff2e30062
pradiphande <pradip.hande@hitachids.com>
2025-03-21 10:29:27 +0530

    pages added

---
+++ src/components/VRMadditionalParameters.js
@@ -0,0 +1,113 @@
+import { HvCard, HvCardContent, HvTypography } from '@hitachivantara/uikit-react-core'
+import { Box } from '@mui/material'
+import React from 'react'
+
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+
+// Styled components for Material-UI table
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: "#000", // Black header
+      color: "#fff", // White text
+      fontWeight: "bold",
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+      color: "#000", // Black text for values
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: "#f7f7f7", // Light gray background for odd rows
+    },
+    "&:nth-of-type(even)": {
+      backgroundColor: "#ffffff", // White background for even rows
+    },
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+  
+ 
+
+const VRMadditionalParameters = () => {
+  return (
+    <Box>
+        <HvCard bgcolor='white' style={{borderRadius: "0px",boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} statusColor="red"> 
+            <HvCardContent>
+                <HvTypography variant="title3">System Settings Supervisor View</HvTypography>
+                <TableContainer component={Paper} elevation={0} style={{ width: "100%", overflowX: "auto" }} >
+                <Table sx={{ minWidth: "100%" }} aria-label="customized table">
+                  <TableBody>
+                    
+                  <StyledTableRow >
+                        <StyledTableCell component="th" scope="row">
+                          DET 1 STATUS
+                        </StyledTableCell>
+                        <StyledTableCell align="right">00</StyledTableCell>
+                      </StyledTableRow>
+                      <StyledTableRow >
+                        <StyledTableCell component="th" scope="row">
+                          DET 2 STATUS
+                        </StyledTableCell>
+                        <StyledTableCell align="right">00</StyledTableCell>
+                      </StyledTableRow>
+                      <StyledTableRow >
+                        <StyledTableCell component="th" scope="row">
+                          DET 1 BGHIGH
+                        </StyledTableCell>
+                        <StyledTableCell align="right">00</StyledTableCell>
+                      </StyledTableRow>
+
+                      <StyledTableRow >
+                        <StyledTableCell component="th" scope="row">
+                          DET 1 BGLOW
+                        </StyledTableCell>
+                        <StyledTableCell align="right">00</StyledTableCell>
+                      </StyledTableRow>
+                      <StyledTableRow >
+                        <StyledTableCell component="th" scope="row">
+                          DET 1 BGLOW
+                        </StyledTableCell>
+                        <StyledTableCell align="right">00</StyledTableCell>
+                      </StyledTableRow>
+                      <StyledTableRow >
+                        <StyledTableCell component="th" scope="row">
+                          DET 1 BGLOW
+                        </StyledTableCell>
+                        <StyledTableCell align="right">00</StyledTableCell>
+                      </StyledTableRow>
+                      <StyledTableRow >
+                        <StyledTableCell component="th" scope="row">
+                          DET 1 BGLOW
+                        </StyledTableCell>
+                        <StyledTableCell align="right">00</StyledTableCell>
+                      </StyledTableRow>
+                      <StyledTableRow >
+                        <StyledTableCell component="th" scope="row">
+                          DET 1 BGLOW
+                        </StyledTableCell>
+                        <StyledTableCell align="right">00</StyledTableCell>
+                      </StyledTableRow>
+            
+                  </TableBody>
+                </Table>
+              </TableContainer>
+                
+            </HvCardContent>
+        </HvCard>
+    </Box>
+  )
+}
+
+export default VRMadditionalParameters
