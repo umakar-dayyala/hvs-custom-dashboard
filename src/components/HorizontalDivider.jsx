1fbc154cb1def380ca39fd97fc43644abcb61a4e
umakar-dayyala <umakar.dayyala@hitachids.com>
2025-05-08 17:37:06 +0530

    Change the border colored

--- src/components/HorizontalDivider.jsx
+++ src/components/HorizontalDivider.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Divider } from "@mui/material";
 
 const HorizontalDivider = () => (
-  <Divider style={{ border: "1px solid #70707059", margin: "1rem" }} />
+  <Divider style={{ border: "1px solid #70707059", margin: "10px", width:"100%" }} />
 );
 
 export default HorizontalDivider;
